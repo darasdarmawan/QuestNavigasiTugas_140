d1f4a11304230fa5405d07ff2533139e5e98100d
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 17:30:13 +0700

    tambah scaffold dan column

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -1,11 +1,19 @@
 package com.example.tugas5.view
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
+    Scaffold (
+        content = { paddingValues ->
+            Column (
 
+            )
+        }
+    )
 }
