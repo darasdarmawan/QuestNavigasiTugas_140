f5c95ee0b44ba6dc9f81c01dadda4262258a7dca
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 18:15:30 +0700

    tambah main column

--- app/src/main/java/com/example/tugas5/view/Formisi.kt
+++ app/src/main/java/com/example/tugas5/view/Formisi.kt
@@ -1,5 +1,11 @@
 package com.example.tugas5.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -10,10 +16,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import  com.example.tugas5.R
 
@@ -43,6 +53,18 @@ fun FormisiScreen(navController: NavController) {
                 )
             )
         },
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.light_purple_bg))
+                    .padding(paddingValues)
+                    .padding(horizontal = 24.dp, vertical = dimensionResource(id = R.dimen.padding_medium)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
 
+            }
+        }
     )
 }
