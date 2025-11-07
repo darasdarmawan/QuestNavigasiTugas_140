a1bd6c089883a4b4a52d28a9703ca43acbc6e3d3
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 18:50:17 +0700

    tambah list of

--- app/src/main/java/com/example/tugas5/view/Tampildata.kt
+++ app/src/main/java/com/example/tugas5/view/Tampildata.kt
@@ -2,12 +2,19 @@ package com.example.tugas5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.example.tugas5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilDataScreen(
     navController: NavController
 ) {
-
+    val dataEntries = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Dara Darmawan"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Perempuan"),
+        Pair("STATUS PERKAWINAN", "Lajang"),
+        Pair(stringResource(id = R.string.alamat), "Yogyakarta")
+    )
 }
