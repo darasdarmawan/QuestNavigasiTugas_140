9e984db2947232643955b6adf570b3f5dac98ea7
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 18:48:09 +0700

    tambah function tampil data screen

---
+++ app/src/main/java/com/example/tugas5/view/Tampildata.kt
@@ -0,0 +1,13 @@
+package com.example.tugas5.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilDataScreen(
+    navController: NavController
+) {
+
+}
