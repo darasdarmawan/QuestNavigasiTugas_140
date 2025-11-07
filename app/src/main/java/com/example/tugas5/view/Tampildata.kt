37508604798c823e9c1e5a814be59fbd039b2745
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 19:03:56 +0700

    tambah function tampildata screen preview

--- app/src/main/java/com/example/tugas5/view/Tampildata.kt
+++ app/src/main/java/com/example/tugas5/view/Tampildata.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -25,9 +26,11 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.tugas5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -143,5 +146,13 @@ fun TampilDataScreen(
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TampilDataScreenPreview() {
+    MaterialTheme {
+        com.example.tugas5.view.TampilDataScreen(navController = rememberNavController())
     }
 }
