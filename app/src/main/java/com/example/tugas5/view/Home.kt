cfb0c87ac532538eb14456c454b7494952b60916
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 17:49:43 +0700

    tambah text selamat datang

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -1,6 +1,5 @@
 package com.example.tugas5.view
 
-import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,13 +7,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.tugas5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,6 +33,13 @@ fun HomeScreen(navController: NavController) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
+                Text(
+                    text = "Selamat Datang",
+                    fontSize = 32.sp,
+                    fontFamily = FontFamily.Monospace,
+                    color = colorResource(id = R.color.purple_500),
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
 
             }
         }
