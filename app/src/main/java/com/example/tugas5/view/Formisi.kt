52c53356609c5ef239b5a49de92703d2fa9286d8
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 18:12:52 +0700

    tambah txt ditop app bar dan warna top app bar

--- app/src/main/java/com/example/tugas5/view/Formisi.kt
+++ app/src/main/java/com/example/tugas5/view/Formisi.kt
@@ -2,13 +2,20 @@ package com.example.tugas5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavController
+import  com.example.tugas5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,7 +30,19 @@ fun FormisiScreen(navController: NavController) {
 
     Scaffold (
         topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Formulir Pendaftaran",
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.ungu_terang)
+                )
+            )
+        },
 
-        }
     )
 }
