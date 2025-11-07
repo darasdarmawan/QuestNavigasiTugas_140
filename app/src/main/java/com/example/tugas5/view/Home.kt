d1e1f024e3c7915e2af1b5097de0b0001660ff50
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 17:43:39 +0700

    tambah resources warna

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -1,9 +1,19 @@
 package com.example.tugas5.view
 
+import android.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -12,8 +22,16 @@ fun HomeScreen(navController: NavController) {
     Scaffold (
         content = { paddingValues ->
             Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorResource(id = R.color.light_purple))
+                    .padding(paddingValues)
+                    .padding(horizontal = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
 
-            )
+            }
         }
     )
 }
