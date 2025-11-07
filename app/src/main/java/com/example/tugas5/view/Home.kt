74d5aa4d89a19950d68ffba0091e178188e85fea
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 18:04:31 +0700

    tambah function HomeScreenPreview

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -23,9 +23,11 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.tugas5.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -76,4 +78,10 @@ fun HomeScreen(navController: NavController) {
             }
         }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    com.example.tugas5.view.HomeScreen(navController = rememberNavController())
 }
