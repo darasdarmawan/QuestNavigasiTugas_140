3341e9eca3f25882c5f741827f9b0efa32be0b49
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 18:08:28 +0700

    tambah mutable state of dan list of

--- app/src/main/java/com/example/tugas5/view/Formisi.kt
+++ app/src/main/java/com/example/tugas5/view/Formisi.kt
@@ -2,10 +2,21 @@ package com.example.tugas5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormisiScreen(navController: NavController) {
-
+    val context = LocalContext.current
+    var nama by remember { mutableStateOf("") }
+    val genderList = listOf("Laki-laki", "Perempuan")
+    var jenisKelamin by remember { mutableStateOf("") }
+    val statusList = listOf("Janda", "Lajang", "Duda")
+    var status by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
 }
