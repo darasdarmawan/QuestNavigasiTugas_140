9589e147dee25b55b45652ae8e5d797c49f27309
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-07 17:53:50 +0700

    tambah image untuk home screen

--- app/src/main/java/com/example/tugas5/view/Home.kt
+++ app/src/main/java/com/example/tugas5/view/Home.kt
@@ -1,10 +1,12 @@
 package com.example.tugas5.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,7 +43,13 @@ fun HomeScreen(navController: NavController) {
                     color = colorResource(id = R.color.purple_500),
                     modifier = Modifier.padding(bottom = 32.dp)
                 )
-
+                Image(
+                    painter = painterResource(id = R.drawable.aurora),
+                    contentDescription = "App Logo",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(bottom = 16.dp)
+                )
             }
         }
     )
